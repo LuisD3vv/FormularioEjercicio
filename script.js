a837d0c9d888a5a943f66f19b70acba53d6561cb
Lissandro <luismexico2006@gmail.com>
2025-09-20 18:13:19 -0700

    Se refactorizo logica de javascript

--- script.js
+++ script.js
@@ -1,72 +1,94 @@
 const alerta = document.getElementById('miAlerta');
 
-
 const datos = {
     nombre: '',
     apellido: '',
     email: '',
-    mensaje:''
+    solicitud: '',
+    mensaje: '',
+    consentimiento: ''
 };
-let boton = document.querySelector('.formulario');
-let nombre = document.querySelector('#name');
-let apellido = document.querySelector('#lastname');
-let email = document.querySelector('#email');
-let mensaje = document.querySelector('#sms');
 
-
-
-nombre.addEventListener('input',leer);
-apellido.addEventListener('input',leer);
-email.addEventListener('input',leer);
-mensaje.addEventListener('input',leer);
-
-
-function leer(e){
+console.log(datos);
+function leer(e) {
     datos[e.target.id] = e.target.value;
+    // asi imprimimos el valor actualizado de cada evento.
+    // en lugar de uno estatico
+    console.log(datos[e.target.id]); 
 }
 
-function validarFormulario (e) {
-    e.preventDefault();
-    const {nombre, apellido, email, mensaje} = datos;
+// Aqui van los elementos a verificar
+const formulario = document.querySelector('.formulario');
+const nombre = document.querySelector('#nombre');
+const apellido = document.querySelector('#apellido');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const solicitud = document.querySelectorAll(".request");
+const consentimiento = document.querySelector("#check");
 
-    console.log(nombre,apellido,email,mensaje)
-    if (!nombre || !apellido || !email || !mensaje) {
+// mandar elemento a funcion
+nombre.addEventListener('input', leer);
+apellido.addEventListener('input', leer);
+email.addEventListener('input', leer);
+mensaje.addEventListener('input', leer);
+solicitud.forEach((e) => {
+    e.addEventListener('click', leer);
+})
+consentimiento.addEventListener('click', leer);
+
+function exito(){
+    alerta.style.display = 'block';
+    alerta.textContent = "Gracias por llenar el cuestionario, nos contactaremos en breve contigo mediante el correo proporcianado";
+}
+
+// validar formulario
+formulario.addEventListener('submit',function(e) {
+    e.preventDefault();
+    let {nombre,apellido,email,solicitud,mensaje,consentimiento} = datos;
+    let UnoPorUno = [nombre,apellido,email,solicitud,mensaje,consentimiento];
+    if (!nombre &&  !apellido && !email && !solicitud && !mensaje && !consentimiento) {
         alerta.style.display = 'block';
-        let camp1 = document.querySelector('#err1')
-        camp1.textContent = 'Campo obligatorio';
-        let camp2 = document.querySelector('#err2');
-        camp2.textContent = 'Campo obligatorio';
-        let camp3 = document.querySelector('#err3');
-        camp3.textContent = 'Campo obligatorio';
-        let camp4 = document.querySelector('#err4');
-        camp4.textContent = 'Porfavor, selecciona una solicitud';
-        let camp5 = document.querySelector('#err5');
-        camp5.textContent = 'Campo obligatorio';
-        let camp6 = document.querySelector('#err6');
-        camp6.textContent = 'Para continuar, es necesario que des consentimiento a ser contactado';
-        const error = [camp1, camp2, camp3, camp4, camp5, camp6];
-        error.forEach((dato) => {
-            dato.style.color = 'red';
-            dato.style.marginTop = '1rem';
-            dato.style.marginLeft = '.5rem';
-        });
-        let inputs = document.querySelectorAll('input');
-        inputs.forEach(input => {
-            document.querySelector('.query1').style.border = "1px solid red";
-            document.querySelector('.query2').style.border = "1px solid red";
-            input.style.border = "1px solid red";
+        let err1,err2,err3,err4,err5,err6;
+        err1 = document.querySelector("#err1");
+        err2 = document.querySelector("#err2");
+        err3 = document.querySelector("#err3");
+        err4 = document.querySelector("#err4");
+        err5 = document.querySelector("#err5");
+        err6 = document.querySelector("#err6");
+        const cambioError = [err1,err2,err3,err4,err5,err6];
+        cambioError.forEach((e) => {
+            e.style.color = 'red';
+            e.style.marginTop = '1rem';
+            e.style.marginLeft = '.5rem';
+            e.textContent = "campo obligatorio";
         });
         return;
     }
-    alert("Mensaje Enviado!\nGracias por completar el formulario, nos pondremos en contacto pronto.");
-}
-
-boton.addEventListener('submit',validarFormulario);
-
-
+    console.log("Informacion enviada");
+    
+    exito();
+    setTimeout(()=> {
+        alerta.remove();
+    },3000);
+    
+});
 
 let cerrar = document.querySelector('#cerrar');
-
-cerrar.addEventListener('click', () => {
-    window.location.reload();
-})
+cerrar.addEventListener('click', (e) => {
+    e.preventDefault();
+    alerta.style.display = 'none';
+    let err1,err2,err3,err4,err5,err6;
+    err1 = document.querySelector("#err1");
+    err2 = document.querySelector("#err2");
+    err3 = document.querySelector("#err3");
+    err4 = document.querySelector("#err4");
+    err5 = document.querySelector("#err5");
+    err6 = document.querySelector("#err6");
+    const cambioError = [err1,err2,err3,err4,err5,err6];
+    cambioError.forEach((e) => {
+        e.style.color = 'unset';
+        e.style.marginTop = '0';
+        e.style.marginLeft = '0';
+        e.textContent = "";
+    });
+});
